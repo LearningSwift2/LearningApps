4ebb23f7186f0e25380ae52c2c5091a2c5a406bc
Phil Wright <touchopia@me.com>
2015-12-12 15:07:50 -0700

    adding simple reminders

---
+++ SimpleReminders/SimpleReminders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleReminders
+//
+//  Created by Phil Wright on 12/8/15.
+//  Copyright © 2015 Touchopia, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
