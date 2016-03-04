b5aed8015a925fd9f160e69b8af20f15c2ae7023
Phil Wright <touchopia@me.com>
2016-03-03 18:48:50 -0700

    Updating CoreData

--- SimpleCoreData/SimpleCoreData/ViewController.swift
+++ SimpleCoreData/SimpleCoreData/ViewController.swift
@@ -2,24 +2,89 @@
 //  ViewController.swift
 //  SimpleCoreData
 //
-//  Created by Phil Wright on 3/1/16.
+//  Created by Phil Wright on 3/3/16.
 //  Copyright © 2016 The Iron Yard. All rights reserved.
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+        let moc = DataController().managedObjectContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        seedPersonPlusStarship()
+        
+        fetch()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    func fetch() {
+        let fetchPerson = NSFetchRequest(entityName: "Person")
+        let fetchStarship = NSFetchRequest(entityName: "Starship")
+        
+        do {
+            let fetchedPersons = try moc.executeFetchRequest(fetchPerson) as! [Person]
+
+            for person in fetchedPersons {
+                if let name = person.name {
+                    print(name)
+                }
+            }
+            
+        } catch {
+            fatalError("failure to fetch person \(error)")
+        }
+        
+        do {
+            let fetchedStarships = try moc.executeFetchRequest(fetchStarship) as! [Starship]
+            
+            for ship in fetchedStarships {
+                if let name = ship.name {
+                    print(name)
+                }
+            }
+        } catch {
+            fatalError("failure to fetch starship \(error)")
+        }
     }
 
+    func seedPersonPlusStarship() {
+        
+
+        
+        let entity = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: moc) as! Person
+        
+        let today = NSDate()
+        
+        entity.setValue("Luke Skywalker", forKey: "name")
+        entity.setValue("Male", forKey: "gender")
+        entity.setValue(today, forKey: "created")
+        
+        let entityStarship = NSEntityDescription.insertNewObjectForEntityForName("Starship", inManagedObjectContext: moc) as! Starship
+        
+        entityStarship.setValue("Melennium Falcon", forKey: "name")
+        entityStarship.setValue(today, forKey: "created")
+        
+        let entityStarship2 = NSEntityDescription.insertNewObjectForEntityForName("Starship", inManagedObjectContext: moc)
+        
+        entityStarship2.setValue("X-wing", forKey: "name")
+        entityStarship2.setValue(today, forKey: "created")
+        
+        
+        do {
+            try moc.save()
+            
+        } catch {
+            
+            fatalError("failure to save context \(error)")
+        }
+        
+        
+    }
 
 }
 
